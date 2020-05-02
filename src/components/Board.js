7b95a0fe583ffab79b97565d57e87cbfd0917fb5
Razielini <razielco@gmail.com>
2020-05-02 00:01:07 -0600

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import Token from './Token'
 
 export default class Board extends Component {
+  renderToken (i) {
+    return <Token
+            value={ this.props.token[i] }
+            onClick={ ()=> this.props.onClick(i) }
+            onMouseEnter={ () => this.props.onMouseEnter() }
+            onMouseLeave={ () => this.props.onMouseLeave() }
+          />
+  }
+
   render () {
     return (
       <div>
-        Board.JS
+        <div className="border-row">
+          { this.renderToken(0) }
+          { this.renderToken(1) }
+          { this.renderToken(2) }
+        </div>
+        <div className="border-row">
+          { this.renderToken(3) }
+          { this.renderToken(4) }
+          { this.renderToken(5) }
+        </div>
+        <div className="border-row">
+          { this.renderToken(6) }
+          { this.renderToken(7) }
+          { this.renderToken(8) }
+        </div>
       </div>
     )
   }
