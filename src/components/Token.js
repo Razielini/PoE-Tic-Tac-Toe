09a077d5852ecb7c0a1bc05679c649c49e158ac0
Razielini <razielco@gmail.com>
2020-05-02 09:57:17 -0600

    Update Token.js
    Now support hover

--- src/components/Token.js
+++ src/components/Token.js
@@ -2,18 +2,45 @@ import React, { Component } from 'react'
 
 // export default function Token(props) {
 export default class Token extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      xIsHover: false
+    }
+  }
+
+  handleHover (currentPlayer, type) {
+    const xIsHover = type === 'Enter' ? true : false
+    this.setState({
+      xIsHover: xIsHover
+    })
+    console.log('currentPlayer :: TOKEN ::', currentPlayer)
+    console.log('isHover :: TOKEN ::', xIsHover)
+    console.log('type :: TOKEN ::', type)
+  }
+
   render () {
-    return (
-      <button
+    const val = this.props.value
+    const currentPlayer = this.props.currentPlayer
+    const xIsHover = this.state.xIsHover
+    return <button
         className="token"
         onClick={ this.props.onClick }
-        onMouseEnter={ this.props.onMouseEnter }
-        onMouseLeave={ this.onMouseLeave }
+        onMouseEnter={ () => this.handleHover(currentPlayer, 'Enter') }
+        onMouseLeave={ () => this.handleHover(currentPlayer, 'Leave') }
       >
-        <img
-          src={ this.props.value }
-        />
+        { val ? <img
+              src={ this.props.value }
+              alt="token"
+            /> : null
+        }
+
+        { xIsHover ? <img
+              src={ currentPlayer }
+              alt="hover token"
+              className="hover-image"
+            /> : null
+        }
       </button>
-    )
   }
 }
