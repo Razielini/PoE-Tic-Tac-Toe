37f21ebb2cea514b36d4f46f30ea6d2c7f5c13ca
Razielini <razielco@gmail.com>
2020-05-02 09:56:43 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
 import Game from './components/Game'
-import Config from './components/Config'
 
 function App() {
   return (
-    <div>
-      <Config />
-      <Game />
-    </div>
+    <Game />
   );
 }
 
