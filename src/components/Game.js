bb5a1de0f5638992126e2b67ea37f95bdba43ff6
Razielini <razielco@gmail.com>
2020-05-02 00:01:05 -0600

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,17 +16,59 @@ export default class Game extends Component {
       stepNumber: 0,
       history: [
         {
-          squares: Array(9).fill(null)
+          token: Array(9).fill(null)
         }
       ]
     }
   }
 
+  handleClick (i) {
+    console.log(`handleClick[${i}] ::`)
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const current = history[history.length - 1]
+    const token = current.token.slice()
+
+    token[i] = this.state.xIsNext ? '/img/CurrencyVaal.png' : '/img/CurrencyRerollRare.png'
+    console.log(`token[${i}] :: ${token[i]}`)
+
+    this.setState({
+      history: history.concat({
+        token
+      }),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    })
+  }
+
+  handleHover (i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const current = history[history.length - 1]
+    const token = current.token.slice()
+
+    token[i] = this.state.xIsNext ? '/img/CurrencyVaal.png' : '/img/CurrencyRerollRare.png'
+    console.log(`handleHover[${i}] ::`)
+
+    this.setState({
+      history: history.concat({
+        token
+      }),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    })
+  }
+
   render () {
+    const history = this.state.history
+    const current = history[this.state.stepNumber]
+
     return (
       <div>
         Hello World
-        <Board />
+        <Board
+          onClick={ (i) => this.handleClick(i) }
+          token={ current.token }
+          onMouseEnter={ (i) => this.handleHover(i) }
+        />
         <pre>
           { this.state.icon.playerA }
           { this.state.icon.playerB }
