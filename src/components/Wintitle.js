fb2100f06d79cdcde341c0da096def5daf1d66f4
Razielini <razielco@gmail.com>
2020-05-04 09:28:15 -0600

    Update Wintitle.js
    Title of winner & play again button

--- src/components/Wintitle.js
+++ src/components/Wintitle.js
@@ -4,13 +4,25 @@ export default function Wintitle(props) {
   console.log('wintitle :: ', props.currentPlayer)
   return (
     <div
-      className={`win-title ${ props.winner === null ? 'visibility-hidden' : null }`}
+      className={props.winner === null ? 'visibility-hidden' : null }
     >
-      {`The ${ props.currentPlayer.name } Win!`}
-      <img
-        src={ props.currentPlayer.img }
-        alt={ props.currentPlayer.name }
-      />
+      <div
+        className="win-title"
+      >
+        {`The ${ props.currentPlayer.name } Win!`}
+        <img
+          src={ props.currentPlayer.img }
+          alt={ props.currentPlayer.name }
+        />
+      </div>
+      <div
+        className="play-again cursor-pointer"
+        onClick={ props.onClick }
+      >
+        <div>
+          Play Again
+        </div>
+      </div>
     </div>
   );
 }
