8b2dc5d2dc6fe55ad0c5bd5199fb46124a5df41d
Razielini <razielco@gmail.com>
2020-05-04 22:39:19 -0600

    Update Wintitle.js
    Draw Game!

--- src/components/Wintitle.js
+++ src/components/Wintitle.js
@@ -1,20 +1,42 @@
 import React from 'react'
 
 export default function Wintitle(props) {
-  console.log('wintitle :: ', props.currentPlayer)
+  console.log('wintitle :: ', props.token)
+  const winner = props.winner
+  const draw = props.token.filter(Boolean).length;
+  console.log('draw :: ', draw)
+
+  let klass = 'visibility-hidden'
+
+  if(draw === 9 && winner === null) {
+    klass = ''
+  }
+
+  if(winner !== null) {
+    klass = ''
+  }
+
   return (
     <div
-      className={props.winner === null ? 'visibility-hidden' : null }
+      className={ klass }
     >
+
+    {
+      draw === 9 && winner === null ?
       <div
         className="win-title"
       >
-        {`The ${ props.currentPlayer.name } Win!`}
-        <img
-          src={ props.currentPlayer.img }
-          alt={ props.currentPlayer.name }
-        />
-      </div>
+        Draw Game!
+      </div> : <div
+                  className="win-title"
+                >
+                  {`The ${ props.currentPlayer.name } Win!`}
+                  <img
+                    src={ props.currentPlayer.img }
+                    alt={ props.currentPlayer.name }
+                  />
+                </div>
+    }
       <div
         className="play-again cursor-pointer"
         onClick={ props.onClick }
